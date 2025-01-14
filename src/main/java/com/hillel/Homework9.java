1e2b58ffd9046bccc7e1ef9ac71564ec411d0419
Myroslava <miraddnk@gmail.com>
2025-01-14 14:56:18 +0200

    added hw 14

--- src/main/java/com/hillel/Homework9.java
+++ src/main/java/com/hillel/Homework9.java
@@ -3,20 +3,24 @@ package com.hillel;
 public class Homework9 {
 
     public static void main(String[] args) {
-        Person person1 = new Man("John", 30, PersonRole.ENGINEER);
-        Person person2 = new Woman("Mary", 25, PersonRole.TEACHER);
-        Person person3 = new Man("Bob", 35, PersonRole.DOCTOR);
+        try {
+            Person person1 = new Man("John", 30, PersonRole.ENGINEER);
+            Person person2 = new Woman("", 20, PersonRole.TEACHER);
+            Person person3 = new Man("Bob", 35, PersonRole.DOCTOR);
 
-        person1.displayInformation();
-        person2.displayInformation();
-        person3.displayInformation();
+            person1.displayInformation();
+            person2.displayInformation();
+            person3.displayInformation();
 
-        person2.changeProfession(PersonRole.DATA_SCIENTIST);
+            person2.changeProfession(PersonRole.DATA_SCIENTIST);
 
-        System.out.println("Updated Information:");
-        person2.displayInformation(true);
+            System.out.println("Updated Information:");
+            person2.displayInformation(true);
 
-        System.out.println("Simplified Information:");
-        person2.displayInformation(false);
+            System.out.println("Simplified Information:");
+            person2.displayInformation(false);
+        } catch (InvalidPersonDataException invalidPersonDataException) {
+            invalidPersonDataException.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/com/hillel/InvalidPersonDataException.java
@@ -0,0 +1,7 @@
+package com.hillel;
+
+public class InvalidPersonDataException extends Exception{
+    public InvalidPersonDataException(String message) {
+        super(message);
+    }
+}
