cc691fb777c76c19a83e2bd967deb631218fe411
Myroslava <miraddnk@gmail.com>
2025-03-10 15:39:41 +0200

    added hw_28

--- src/main/java/com/hillel/BrowserFactory.java
+++ src/main/java/com/hillel/BrowserFactory.java
@@ -8,42 +8,71 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 public class BrowserFactory {
 
-    private static WebDriver driver;
+	private static WebDriver driver;
 
-    public static void createDriver(String browserName) {
-        switch (browserName.toLowerCase()) {
-            case ("chrome"):
-                WebDriverManager.chromedriver().setup();
-                Map<String, Object> prefs = new HashMap<>();
-                prefs.put("profile.default_content_settings.popups", 0);
-                prefs.put("download.default_directory", ConfigReader.getProperty("download.dir"));
+	public static void createDriver(String browserName) {
+		switch (browserName.toLowerCase()) {
+			case ("chrome"):
+				WebDriverManager.chromedriver().setup();
+				Map<String, Object> prefs = new HashMap<>();
+				prefs.put("profile.default_content_settings.popups", 0);
+				prefs.put("download.default_directory", ConfigReader.getProperty("download.dir"));
 
-                ChromeOptions options = new ChromeOptions();
-                options.setExperimentalOption("prefs", prefs);
-                driver = new ChromeDriver(options);
-                driver.manage().window().maximize();
-                WebDriverRunner.setWebDriver(driver);
-                break;
-            case ("firefox"):
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                break;
-            default:
-                throw new InvalidArgumentException(String.format("Unsupported browser " + browserName));
-        }
-    }
+				ChromeOptions options = new ChromeOptions();
+				options.setExperimentalOption("prefs", prefs);
+				options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+					put("sessionTimeout", "15m");
+					put("enableVideo", true);
+				}});
 
-    public static WebDriver getDriver() {
-        return driver;
-    }
+				//for local launch
+				driver = new ChromeDriver(options);
 
-    public static void quitDriver() {
-        driver.quit();
-    }
+				//for remote launch
+				try {
+					driver = new RemoteWebDriver(new URL(ConfigReader.getProperty("remote.url")), options);
+				} catch (MalformedURLException e) {
+					throw new RuntimeException();
+				}
+
+				driver.manage().window().maximize();
+				WebDriverRunner.setWebDriver(driver);
+				break;
+			case ("firefox"):
+				WebDriverManager.firefoxdriver().setup();
+				FirefoxOptions firefoxOptions = new FirefoxOptions();
+
+				//for local launch
+				driver = new FirefoxDriver();
+
+				//for remote launch
+				try {
+					driver = new RemoteWebDriver(new URL(ConfigReader.getProperty("remote.url")), firefoxOptions);
+				} catch (MalformedURLException e) {
+					throw new RuntimeException();
+				}
+
+				break;
+			default:
+				throw new InvalidArgumentException(String.format("Unsupported browser " + browserName));
+		}
+	}
+
+	public static WebDriver getDriver() {
+		return driver;
+	}
+
+	public static void quitDriver() {
+		driver.quit();
+	}
 }
